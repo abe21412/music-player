dad4f143e1dfc5afcbf28f95da41cb3caabd38d2
abe garcia <44239562+abe21412@users.noreply.github.com>
2019-05-29 20:20:31 -0400

    edited package.json

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const path = require("path");
 const { verifyToken } = require("./controllers/auth"); //middleware for protected routes
 const userRouter = require("./routers/users");
 const authRouter = require("./routers/auth");
@@ -12,6 +13,7 @@ const trackRouter = require("./routers/tracks");
 const app = express();
 const port = 8080;
 //middleware
+app.use(express.static(path.join(__dirname, "client", "build")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
@@ -21,4 +23,8 @@ app.use("/api/auth", authRouter);
 app.use("/api/users", verifyToken, userRouter);
 app.use("/api/tracks", verifyToken, trackRouter);
 
+//serve react build
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 app.listen(port, () => console.log(`listening on port ${port}`));
